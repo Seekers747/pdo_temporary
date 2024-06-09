9b0cc60595e5b7c15aedf686f31f73385c2a9c37
imraan mohamehdoesein <2194529@talnet.nl>
2024-06-09 21:33:28 +0200

    werkende update

--- opdracht-5/update.php
+++ opdracht-5/update.php
@@ -2,24 +2,27 @@
 
 require 'db.php';
 
+$product_code = '';
 $product_naam = '';
 $prijs = '';
 $omschrijving = '';
 
 if (isset($_POST['versturen'])) {
+    $product_code = $_POST['product_code'];
     $product_naam = $_POST["product_naam"];
     $prijs = $_POST["prijs_per_stuk"];
     $omschrijving = $_POST["omschrijving"];
-}
 
-$query='insert into producten (product_naam, prijs_per_stuk, omschrijving) values (?, ?, ?)';
-$result=$pdo->prepare($query);
-$data=array($product_naam, $prijs, $omschrijving);
-$result->execute($data);
+    $sql = "UPDATE producten SET product_naam=?, prijs_per_stuk=?, omschrijving=? WHERE product_code=?";
+    $stmt= $pdo->prepare($sql);
+    $stmt->execute([$product_naam, $prijs, $omschrijving, $product_code]);
+}
 
 ?>
 
 <form method="post">
+    <label>vul hier de id van welke product wil veranderen en wat het moet worden:</label><br>
+    <input type="number" name="product_code" placeholder="id"><br>
     <input type="text" name="product_naam" placeholder="product naam"><br>
     <input type="number" name="prijs_per_stuk" placeholder="prijs"><br>
     <input type="text" name="omschrijving" placeholder="omschrijving"><br>
